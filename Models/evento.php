de391d701ea609fdb325fc3c854954e49f089e1e
santiago <santiaadev98@gmail.com>
2020-06-08 18:18:48 -0500

    arreglos en la clase evento, para poder insertar uno nuevo

--- Models/evento.php
+++ Models/evento.php
@@ -13,7 +13,19 @@
             $conexion->query(" INSERT INTO evento (lugar, requisitos, fecha, 
                                                    hora, cedula_entrenador) 
                                             VALUE ('$evento->lugar', '$evento->requisitos', '$evento->fecha', 
-                                                   '$evento->hora', '$evemto->cedula_entrenador') ");
+                                                   '$evento->hora', '$evento->cedulaEntrenador') ");
+            $idEvento = mysqli_insert_id($conexion);
+            self::agregarAtletasEvento($evento->atletas, $idEvento);
+            echo  json_encode(array('estado' => 'ok'));
+        }
+
+        public function agregarAtletasEvento($cedulaAtletas, $idEvento) {
+            include self::rutaConfig();
+            
+            foreach($cedulaAtletas as $cedula) {
+                $conexion->query(" INSERT INTO mejor_resultado (cedula_atleta, id_evento) 
+                                                        VALUE ('$cedula', '$idEvento') ");
+            }
         }
     }
 ?>
