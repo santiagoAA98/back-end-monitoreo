483d74f876905c6105ba281af7d6aeeb5b315f8d
santiago <santiaadev98@gmail.com>
2020-06-13 14:54:16 -0500

    modificaciones al metodo inicio de sesion

--- Models/sesion.php
+++ Models/sesion.php
@@ -9,17 +9,51 @@
         public function iniciarSesion($usuario, $clave) {
             include self::rutaConfig();
             
-            $sqlSesion = "SELECT usuario, rol FROM sesion WHERE usuario = '$usuario' AND clave='$clave' ";
+            $sqlSesion = "SELECT sesion.usuario, sesion.rol 
+                          FROM sesion
+                          WHERE sesion.usuario = '$usuario' AND sesion.clave='$clave'";
+
             $resultadoSesion = $conexion->query($sqlSesion);
 
             if (($fila = $resultadoSesion->fetch_array())==true) {
-                $data = array(
-                    'usuario' => $fila['usuario'],
-                    'rol' => $fila['rol'],
-                    'estado' => 'ok',
-                );
 
-                echo json_encode($data);
+                $verificarEstado = false;
+
+                if($fila['rol'] === 'administrador') {
+                    $verificarEstado = true;
+                } else if ($fila['rol'] === 'entrenador') {
+                    $sqlPerfil = " SELECT *
+                                   FROM entrenador
+                                   WHERE entrenador.usuario = '$usuario' AND entrenador.estado = 'activo' ";
+
+                    $resultadoPerfil = $conexion->query($sqlPerfil);
+
+                    if (($perfil = $resultadoPerfil->fetch_array())==true) {
+                        $verificarEstado = true;
+                    }
+                } else if ($fila['rol'] === 'atleta') {
+                    $sqlPerfil = " SELECT *
+                                   FROM atleta
+                                   WHERE atleta.usuario = '$usuario' AND atleta.estado = 'activo' ";
+
+                    $resultadoPerfil = $conexion->query($sqlPerfil);
+
+                    if (($perfil = $resultadoPerfil->fetch_array())==true) {
+                        $verificarEstado = true;
+                    }
+                }
+
+                if($verificarEstado) {
+                    $data = array(
+                        'usuario' => $fila['usuario'],
+                        'rol' => $fila['rol'],
+                        'estado' => 'ok',
+                    );
+    
+                    echo json_encode($data);
+                } else {
+                    echo  json_encode(array('estado' => 'error'));
+                }
             } else {
                 echo  json_encode(array('estado' => 'error'));
             }
